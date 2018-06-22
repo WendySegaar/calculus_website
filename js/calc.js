b386d5c82e9ac178804649cbc677f8c6f5e05665
WendySegaar <36224315+WendySegaar@users.noreply.github.com>
2018-06-22 14:51:28 +0200

    Begin js en css is af
    Begin van javascript code en de css is af

--- js/calc.js
+++ js/calc.js
@@ -1,4 +1,14 @@
-function calc(operator) {
-	var colorThis = operator + "Btn";
+function calc(id) {
+	for (var i = 1; i <= 9; i++){
+		document.getElementById(i).style.backgroundColor = "var(--light-grey)";
+	}
+	var colorThis = id;
 	document.getElementById(colorThis).style.backgroundColor = "var(--light-blue)";
+	if(id == 8){
+		document.getElementById("getal1").value = document.getElementById("getal2").value = "";
+	}
+	if(id == 9){
+		var getal1 = document.getElementById("getal1").value;
+		var getal2 = document.getElementById("getal2").value;
+	}
 }
